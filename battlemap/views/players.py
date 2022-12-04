656b60748ffac8f21885557133b88b49fb365aac
Blake <blakelehrbass@gmail.com>
2022-12-04 17:14:18 -0500

    Set up PUT request for players model

--- battlemap/views/players.py
+++ battlemap/views/players.py
@@ -1,5 +1,7 @@
 from battlemap.views.app import app
-import json
+from battlemap.models.database import db_session
+from battlemap.models.players import PlayerModel
+from flask import request, jsonify
 # 3 ways to get data from request object.
 # Anything after "?" is a query string.
 
@@ -11,5 +13,21 @@ def get_players():
 @app.route('/players', methods=['PUT'])
 def create_player():
     """ Creates a new player. """
-    return 'Create player'
+    # Create the player object.
+    name = request.json['name']
+    player = PlayerModel(name=name)
     
+    # Commit the player to the database.
+    db_session.add(player)
+    db_session.commit()
+
+    # Return player data.
+    response = {
+        'player_id': player.id,
+        'player_name': player.name,
+        'player_created': player.created,
+    }
+    return jsonify(response)
+
+    # HOMEWORK!!!!
+    # FINISH THE REST OF THE APIs USING SQLALCHEMY!!!!!!!
