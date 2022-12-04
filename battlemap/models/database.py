1d71fcebb01cdfddd4cfec6145be709ccbb0e244
Blake <blakelehrbass@gmail.com>
2022-12-04 15:59:09 -0500

    Add init_engine method to create engine using environment variables

--- battlemap/models/database.py
+++ battlemap/models/database.py
@@ -2,8 +2,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-engine = create_engine('postgresql://battlemap:battlemap@postgres:5432/battlemap')
+def init_engine():
+    """ This method will create an engine using the appropriate
+    environment variables from the docker-compose file. """
+    user = os.environ['POSTGRES_USER']
+    password = os.environ['POSTGRES_PASSWORD']
+    host = os.environ['POSTGRES_HOST']
+    db = os.environ['POSTGRES_DB']
+    port = os.environ['POSTGRES_PORT']
+    return create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    
+
+engine = init_engine()
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
