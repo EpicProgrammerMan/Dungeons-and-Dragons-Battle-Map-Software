f89471d8c494a18f688681c80b40566a4b5a01fa
Blake <blakelehrbass@gmail.com>
2023-02-12 14:35:24 -0500

    Parameterize unit tests

--- battlemap/views/player.py
+++ battlemap/views/player.py
@@ -7,7 +7,6 @@ from sqlalchemy import select
 
 @app.route('/player/<player_id>', methods=['GET'])
 def get_player(player_id):
-    breakpoint()
     """ Returns the details of a specified player. """
 
     # Gets a single player from the database using its player id.
@@ -22,7 +21,27 @@ def get_player(player_id):
     }
     return jsonify(response)
 
-@app.route('/player/<player_id>', methods=['POST'])
+# Updated to add "GET" before "POST"
+@app.route('/player/<player_id>', methods=['GET', 'POST'])
 def update_player(player_id):
+    # I swear I updated this but it has changed back. Hmmm
     """ Creates a new player. """
-    return f'Update player called with {player_id}'
+
+    # The POST method is meant to change something about the player.
+    # Lets change its name.
+    newName = request.json['name']
+
+    # Gets player from database whose id is the same as the id requested.
+    stmt = select(PlayerModel).where(PlayerModel.id==player_id)
+
+    player = db_session.execute(stmt).first()[0]
+    
+    player.name = newName
+
+    # Now has to put the player back into the database apparently.
+    # This doesn't work here, I want to replace one of the items
+    # in the database. Not add a new one at the end.
+    db_session.add(player)
+    db_session.commit()
+
+    return f'Update player called with {player_id}, set new name to be {newName}'
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,21 @@
-# Simple example pytest script
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(4) == 5
-
 from battlemap.views.app import app
 from battlemap.views import init_api
 init_api()
+import pytest
 
 # https://circleci.com/blog/testing-flask-framework-with-pytest/
-def test_flask(database):
+
+# Run with command: docker-compose exec battlemap poetry run pytest
+# I can try using fixtures.
+TEST = [
+       ( 'put', '/players', '{"name":"king blake the very cool"}' ),
+       ( 'get', '/players', None ),
+       ( 'get', '/player/1', None ),
+       ( 'post', '/player/1', '{"name":"king blake the very cool and epic"}' )
+       ]
+@pytest.mark.parametrize('requestType, endpoint, dataInput', TEST)
+def test_flask(database, requestType, endpoint, dataInput):
     headers = { "Content-Type": "application/json" }
-    response = app.test_client().get('/players', headers=headers)
-    # curl --header "Content-Type: application/json" --request GET http://localhost:8002/player/2
+    method = getattr(app.test_client(), requestType)
+    response = method( endpoint, headers=headers, data=dataInput )
+    assert response.status_code == 200
